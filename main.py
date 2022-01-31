823475febba15a866c23ba685fa39b117fe68ba5
D1Ygrek <49139544+D1Ygrek@users.noreply.github.com>
2022-01-31 23:27:17 +0500

    replacing project to dev server

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import uvicorn
+
 from fastapi import FastAPI
+
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    back_token: str
+    tg_token: str
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
+ 
+app = FastAPI()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=7777, reload = True)
